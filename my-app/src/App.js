7ad3b8266a92f9c9855f6daf9d1258cab0938af5
joanamorais13 <joanamorais13@gmail.com>
2025-03-18 16:29:55 +0000

    added api integration

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,22 @@
 import logo from './logo.png';
 import './App.css';
+import Dictionary from "./Dictionary";
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo"  />
-        
-        
-        
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className='App-footer'>
+        Coded by Joana Morais
+      </footer>
+    </div>
     </div>
   );
 }
